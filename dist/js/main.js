d98dae62b4c2e10b389570b1790754f17cc41758
Dmitrii Matveev <matveyeff@mail.ru>
2020-09-14 14:29:15 +0300

    sliders are added

--- dist/js/main.js
+++ dist/js/main.js
@@ -17,7 +17,7 @@ function initMap() {
   };
   var google = window.google;
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 12,
+    zoom: 13,
     center: {
       lat: 55.7005654,
       lng: 37.6214963
@@ -95,4 +95,16 @@ function initMap() {
 
 document.addEventListener('DOMContentLoaded', function () {
   initMap();
+});
+$('.slider-excellence__list').slick({
+  appendArrows: $('.slider-excellence__btns'),
+  prevArrow: '<button class="slider-excellence__btn slider-excellence__btn_prev"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9.21839 1L1 9L9.21839 17"/></svg></button>',
+  nextArrow: '<button class="slider-excellence__btn slider-excellence__btn_next"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.78161 17L9 9L0.78161 1"/></svg></button>'
+});
+$('.slider-video__list').slick({
+  variableWidth: true,
+  slidesToShow: 2,
+  appendArrows: $('.slider-video__arrows'),
+  prevArrow: '<button class="slider-video__arrow slider-video__arrow_prev"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9.21839 1L1 9L9.21839 17"/></svg></button>',
+  nextArrow: '<button class="slider-video__arrow slider-video__arrow_next"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.78161 17L9 9L0.78161 1"/></svg></button>'
 });
